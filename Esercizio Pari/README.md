c3d6aa02a14c8d5007a8eab9c135e2aee5b3a125
Drew0004 <drdrewgraphic@gmail.com>
2023-12-12 16:21:09 +0100

    Create funzioni

--- Esercizio Pari/README.md
+++ Esercizio Pari/README.md
@@ -1,7 +1,7 @@
 Consegna pari e dispari
 
-L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-Sommiamo i due numeri
+L'utente sceglie pari o dispari e inserisce un numero da 1 a 5. ----------OK
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). ----------OK
+Sommiamo i due numeri ----------OK
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
--- Esercizio Pari/js/scripts.js
+++ Esercizio Pari/js/scripts.js
@@ -1,15 +1,51 @@
+// Assegno bottone
 const submitButton = document.getElementById('special-button');
 
-
-
+// Aggiungo evento al click
 submitButton.addEventListener('click', function(){
     const userSelect = document.getElementById('user-select').value;
     console.log ("L'utente ha inserito " + userSelect, typeof userSelect);
 
-    const userNumber = document.getElementById('user-number').value;
+    const userNumber = parseInt(document.getElementById('user-number').value);
     console.log ("L'utente ha inserito " + userNumber, typeof userNumber);
+
+
+
+
+    let cpuNumber = randomNumber(1,5);
+    console.log(cpuNumber, typeof cpuNumber);
+
+
+
+    const myResult = mySum (userNumber,cpuNumber);
+    console.log ("Il risultato è " + myResult);
 })
 
+// // Creo funzione che genera numeri random
+// function randomNumber(min,max){
+//     return Math.floor(Math.random() * (max- min +1)) + min;
+// }
 
+// let cpuNumber = randomNumber(1,5);
+// console.log(cpuNumber, typeof cpuNumber);
 
+// //Creo funzione che fa la somma
+// function mySum(num1,num2){
+//     const result = num1 + num2;
+//     return result;
+// }
+
+// const myResult = mySum (userNumber,cpuNumber);
+// console.log (myResult);
+
+// Creo funzione che genera numeri random
+function randomNumber(min,max){
+    return Math.floor(Math.random() * (max- min +1)) + min;
+}
+
+//Creo funzione che fa la somma
+function mySum(num1,num2){
+
+    return num1 + num2;
+}
 
